7255a13fa1f839012f9334a5b07a6dbf74b0f3b7
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2025-08-03 09:59:13 +0100

    Fix casing for HeaderStats component import and file

--- src/pages/receptionistpages/overview/index.jsx
+++ src/pages/receptionistpages/overview/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "../../../components/sidebar";
-import HeaderStats from "../../../components/headerstats";
+import HeaderStats from "../../../components/HeaderStats";
+
 import ConsultationForm from "../../../components/consultationform";
 import ReceiptTemplate from "../../../components/receipttemplate";
 
