0e57297ef86885d8f748ec523d450fe3141946c5
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2025-09-20 17:35:42 +0100

    retouch the responsivness of the contact admin page

--- src/pages/authforms/contactadmin.jsx
+++ src/pages/authforms/contactadmin.jsx
@@ -64,11 +64,11 @@ const ContactAdmin = () => {
           <img
             src={logo2}
             alt="Logo"
-            className="h-[40px] w-[166px] ml-4 sm:ml-[140px] mt-6 mb-6"
+            className="h-[40px] w-[166px] ml-4 sm:ml-[140px]"
           />
         </a>
       </div>
-      <div className="bg-[#FFF] p-[24px] sm:p-[40px] rounded-[24px] shadow-md w-[90%] sm:w-[440px] max-w-[440px] mt-[4px] sm:mt-0">
+      <div className="bg-[#FFF] p-[24px] sm:p-[40px] rounded-[24px] shadow-md w-[90%] sm:w-[440px] max-w-[440px] mt-[72px] sm:mt-0">
         <div className="mb-[2px] sm:mb-8 text-[#383F45] text-[13px] sm:text-[14px] font-inter font-normal leading-[20px]">
           <a href="#" onClick={handleBack}>
             ← BACK
